c09f7cfa21dcb2a2ea974ae89bb75638b6b7847c
cheezycode <cheezycode@gmail.com>
2023-02-24 17:14:02 +0530

    Initial commit

---
+++ app/src/androidTest/java/com/cheezycode/quotesviewmodel/MainActivityTest.kt
@@ -0,0 +1,56 @@
+package com.cheezycode.quotesviewmodel
+
+import android.content.Intent
+import androidx.test.espresso.Espresso.*
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.activityScenarioRule
+import org.hamcrest.CoreMatchers.allOf
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+
+class MainActivityTest{
+
+    @get:Rule
+    val activityScenarioRule = activityScenarioRule<MainActivity>()
+
+    @Test
+    fun testNextButton_expectedCorrectQuote(){
+
+        onView(withId(R.id.btnNext)).perform(click())
+        onView(withId(R.id.btnNext)).perform(click())
+        onView(withId(R.id.btnNext)).perform(click())
+        onView(withId(R.id.quoteText)).check(matches(withText("Difficulties increase the nearer we get to the goal.")))
+    }
+
+    @Test
+    fun testShareButton_expectedIntentChooser(){
+        Intents.init()
+        val expected = allOf(hasAction(Intent.ACTION_SEND))
+        onView(withId(R.id.floatingActionButton)).perform(click())
+        intended(expected)
+        Intents.release()
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/cheezycode/quotesviewmodel/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.cheezycode.quotesviewmodel
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+
+class MainActivity : AppCompatActivity() {
+    lateinit var mainViewModel: MainViewModel
+
+    private val quoteText: TextView
+        get() = findViewById(R.id.quoteText)
+
+    private val quoteAuthor: TextView
+        get() = findViewById(R.id.quoteAuthor)
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                MainViewModel(applicationContext)
+            }
+        }
+        mainViewModel = ViewModelProvider(this, Factory).get(MainViewModel::class.java)
+        setQuote(mainViewModel.getQuote())
+
+    }
+
+    fun setQuote(quote:Quote){
+        quoteText.text = quote.text
+        quoteAuthor.text = quote.author
+    }
+
+    fun onPrevious(view: View) {
+        setQuote(mainViewModel.previousQuote())
+    }
+
+    fun onNext(view: View) {
+        setQuote(mainViewModel.nextQuote())
+    }
+
+    fun onShare(view: View) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.setType("text/plain")
+        intent.putExtra(Intent.EXTRA_TEXT, mainViewModel.getQuote().text)
+        startActivity(intent)
+    }
+
+
+}
